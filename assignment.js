8c2911d195b82a04d969763a13f516d62a3580f3
riandev <rian.acca@gmail.com>
2021-01-22 21:48:26 +0600

    added Bonus Part

--- assignment.js
+++ assignment.js
@@ -1,7 +1,11 @@
 // https://github.com/riandev/assignment-3
 // Start Kilometer to Meter Converter
 function kilometerToMeter(kilometer) {
-    var meter = kilometer * 1000;
+    if (kilometer >= 1) {
+        var meter = kilometer * 1000;
+    } else if (kilometer <= 0 || kilometer <= -1) {
+        var meter = 'Please Input Valid Kilometer';
+    }
     return meter;
 }
 // End Kilometer to Meter Converter
@@ -9,12 +13,17 @@ function kilometerToMeter(kilometer) {
 // Start Budget Calculator
 
 function budgetCalculator(watchPurchaseQuantity, phonePurchaseQuantity, laptopPurchaseQuantity) {
-    var watchPrice = 50 * watchPurchaseQuantity;
-    var phonePrice = 100 * phonePurchaseQuantity;
-    var laptopPrice = 500 * laptopPurchaseQuantity;
-    var totolBudget = watchPrice + phonePrice + laptopPrice;
-    return totolBudget;
+    if (watchPurchaseQuantity, phonePurchaseQuantity, laptopPurchaseQuantity >= 1) {
+        var watchPrice = 50 * watchPurchaseQuantity;
+        var phonePrice = 100 * phonePurchaseQuantity;
+        var laptopPrice = 500 * laptopPurchaseQuantity;
+        var totolBudget = watchPrice + phonePrice + laptopPrice;
+    } else {
+        var totalInvalidBudget = 'Please Input Valid Quantity'; // In Case, If user Not Put Valid Quantity
+    }
+    return totolBudget, totalInvalidBudget; // returning two value, One for Valid Quantity, Another for Invalid Quantity
 }
+
 // End Budget Calculator
 
 
@@ -25,19 +34,23 @@ function hotelCost(hotelDayCount) {
     if (hotelDayCount <= 10) {
         totalHotelBill = hotelDayCount * 100;
     } else if (hotelDayCount <= 20) {
-        var HotelRegularBill = 10 * 100;
+        var hotelRegularBill = 10 * 100;
         var remainingDay = hotelDayCount - 10;
         var discountPackageA = remainingDay * 80;
-        totalHotelBill = HotelRegularBill + discountPackageA;
-    } else {
-        var HotelRegularBill = 10 * 100;
+        totalHotelBill = hotelRegularBill + discountPackageA;
+    } else if (hotelDayCount >= 21) {
+        var hotelRegularBill = 10 * 100;
         var discountPackageA = 10 * 80;
         var remainingDay = hotelDayCount - 20;
         var discountPackageB = remainingDay * 50;
-        totalHotelBill = HotelRegularBill + discountPackageA + discountPackageB;
+        totalHotelBill = hotelRegularBill + discountPackageA + discountPackageB;
+    } else {
+        var totalHotelBillInvalid = 'Please Input Valid Day Count';
+        //Instead of minus output now it will undefine.
     }
-    return totalHotelBill;
+    return totalHotelBill, totalHotelBillInvalid;
 }
+
 // End Hotel Cost Calculator
 
 
